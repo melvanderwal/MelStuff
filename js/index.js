c2389990f6fc5a75d61ff5c7eb6e9c49b2a079b5
melvanderwal <melvanderwal@gmail.com>
2021-04-19 15:15:38 +1000

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@ var contentMap = new Map();
 contentMap.set("weather", "https://melvanderwal.github.io/Weather/?padtop=50");
 contentMap.set("radar", "https://melvanderwal.github.io/Radar/?title=false");
 contentMap.set("fuel", "https://melvanderwal.github.io/QueenslandFuelPrices/?title=false");
+contentMap.set("hockey", "https://melvanderwal.github.io/Hockey/?padtop=60");
 
 var iconMap = new Map();
 iconMap.set("weather", "fas fa-cloud-sun-rain");
 iconMap.set("radar", "fas fa-satellite-dish");
 iconMap.set("fuel", "fas fa-gas-pump");
+iconMap.set("hockey", "fas fa-hockey-mask");
 
 const urlParams = new URLSearchParams(window.location.search);
 if (urlParams.has("view"))
@@ -17,9 +19,7 @@ function changeSource(newType, doToggle) {
     let newIcon = iconMap.get(newType);
     let frame = document.getElementById("pageFrame");
     let buttonIcon = document.getElementById("buttonIcon");
-    //if (newSrc !== frame.getAttribute("src")) {
     if (newSrc !== frame.contentWindow.location) {
-        //frame.setAttribute("src", newSrc);
         frame.contentWindow.location.replace(newSrc);
         frame.reload;
         buttonIcon.className = newIcon;
